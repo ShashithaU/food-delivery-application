2e04db5f9917c49fee8fe2245e84cedc703ad8ff
ShashithaU <udeshshashitha@gmail.com>
2025-03-28 08:34:45 +0530

    Service Registry was created

---
+++ eureka/eureka/src/test/java/com/UOK/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UOK.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
